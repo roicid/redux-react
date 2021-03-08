410df94681a7da3228ac4b22c1b3be17bf36aa71
roicid <roi_cid89@hotmail.com>
2021-03-08 18:56:07 +0100

    reactredux

---
+++ src/reducers/isLogged.js
@@ -0,0 +1,12 @@
+const loggedReducer = (state = false , action ) =>  {
+    
+    switch (action.type) {
+        case 'SING_IN' : 
+        return !state ; 
+        default : 
+        return state; 
+    }
+
+};
+
+export default loggedReducer; 
